61f8005be59d7b60617c7f55d7690ebd78d53e0d
Ru Selvadurai <me@itsru.com>
2019-05-07 17:52:54 +0800

    add data models

---
+++ src/app/models/customer.model.ts
@@ -0,0 +1,14 @@
+export interface Customer {
+  id: string;
+  name: string;
+  phone: string;
+  email: string;
+  street: string;
+  city: string;
+  country: string;
+  postcode: string;
+  userId: string;
+  status: string;
+  createdAt?: string;
+  imageUrl?: string;
+}
